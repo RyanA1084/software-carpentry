ea20d434927a2457fe0e5f1e94000b0cc3a59b0f
Presenter <Presenter>
2016-02-05 10:48:20 -0700

    added by ron2

--- middle.sh
+++ middle.sh
@@ -1,4 +1,4 @@
-# This script prints the middle rows of a file
+# This script prints the middle rows of a file, change on line 1
 # Takes arguments:
 # name_of_file number_of_head_lines number of tail_lines
 head -"$2" "$1" | tail -"$3"
